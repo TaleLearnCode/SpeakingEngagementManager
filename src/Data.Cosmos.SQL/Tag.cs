33a8595dc2a80771256ac3da3b050b5855b4f657
Chad Green <chadgreen@chadgreen.com>
2020-09-27 13:07:37 -0400

    Renamed CosmosRepository to Data.Cosmos.SQL

--- src/Data.Cosmos.SQL/Tag.cs
+++ src/Data.Cosmos.SQL/Tag.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.Json.Serialization;
 using TaleLearnCode.SpeakingEngagementManager.Domain;
 
-namespace TaleLearnCode.SpeakingEngagementManager.Repository.CosmosSDK
+namespace TaleLearnCode.SpeakingEngagementManager.Data.Cosmos.SQL
 {
 
 	/// <summary>
