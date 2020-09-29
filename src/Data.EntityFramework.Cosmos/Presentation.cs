41fa9a8ee6bc15d7b871ba12470cef9d2dddd2c7
Chad Green <chadgreen@chadgreen.com>
2020-09-28 22:11:44 -0400

    TaleLearnCode Stream 28-SEP-2020 — Built out the SQL and Cosmos context.

--- src/Data.EntityFramework.Cosmos/Presentation.cs
+++ src/Data.EntityFramework.Cosmos/Presentation.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using TaleLearnCode.SpeakingEngagementManager.Domain;
+﻿using TaleLearnCode.SpeakingEngagementManager.Domain;
 
 namespace TaleLearnCode.SpeakingEngagementManager.Data.EntityFramework.Cosmos
 {
@@ -9,18 +8,8 @@ namespace TaleLearnCode.SpeakingEngagementManager.Data.EntityFramework.Cosmos
 	/// </summary>
 	/// <seealso cref="EntityFramework.Presentation" />
 	/// <seealso cref="IPartitionKey" />
-	public class Presentation : Data.EntityFramework.Presentation, IPartitionKey
+	public class Presentation : EntityFramework.Presentation, IPartitionKey
 	{
-
-		/// <summary>
-		/// Gets or sets the email address of the data owner.
-		/// </summary>
-		/// <value>
-		/// A <c>string</c> representing the data owner's email address.
-		/// </value>
-		[JsonPropertyName(Domain.PropertyNames.PartitionKey.CosmosPartitionKey)]
-		public string OwnerEmailAddress { get; set; }
-
 	}
 
 }
