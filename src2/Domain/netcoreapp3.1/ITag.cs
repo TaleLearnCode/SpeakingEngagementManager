2717509a0fb0623c3e467766c0b290c476fd4f72
Chad Green <chadgreen@chadgreen.com>
2020-10-15 13:47:00 -0400

    Restructured the Domain project to have targeted code in distinguished folders.

---
+++ src2/Domain/netcoreapp3.1/ITag.cs
@@ -0,0 +1,28 @@
+﻿namespace TaleLearnCode.SpeakingEngagementManager.Domain
+{
+
+	/// <summary>
+	/// Interface for objects representing a tag for a presentation.
+	/// </summary>
+	public interface ITag
+	{
+
+		/// <summary>
+		/// Gets the identifier of the tag.
+		/// </summary>
+		/// <value>
+		/// A <c>string</c> representing the tag identifier.
+		/// </value>
+		string Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the tag.
+		/// </summary>
+		/// <value>
+		/// A <c>string</c> representing the tag's name.
+		/// </value>
+		string Name { get; set; }
+
+	}
+
+}
