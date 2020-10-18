ce8d5396561791e54421302369c99281f9c4e288
Chad Green <chadgreen@chadgreen.com>
2020-10-18 12:35:10 -0400

    Removed incorrectly checked-in settings file

--- Console/Setttings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaleLearnCode.SpeakingEngagementManager.ConsoleTaleLearnCode.SpeakingEngagementManager.ConsoleApp
-{
-	public static class Settings
-	{
-
-		public const string CosmosConnectionString = "AccountEndpoint=https://building-cosmos-app.documents.azure.com:443/;AccountKey=o0B6OUEmQdJkvrGitOkMmcUZC8YS0iMQfogAMAso9PVs3Hgjo5PRzOIs85kyGtsI1A9dPAmFAx75pTQp0WhLbA==;";
-
-	}
-}
