372c973aab0b58a4c70bddfed455103c19ad55c5
Chad Green <chadgreen@chadgreen.com>
2020-10-22 12:06:30 -0400

    Rebuilt the change feed monitor into a new Azure Functions project.  Using DI and lambdas.

---
+++ src/ChangeFeedMonitor/DatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace TaleLearnCode.SpeakingEngagementManager.ChangeFeedMonitor
+{
+
+	/// <summary>
+	/// Represents the database settings necessary for the Azure Functions.
+	/// </summary>
+	/// <remarks>
+	/// This is done because the CosmosDBTrigger requires static values be used for its settings.
+	/// </remarks>
+	internal static class DatabaseSettings
+	{
+		internal const string DatabaseName = "newDev";
+		internal const string ContainerName = "new";
+	}
+
+}
